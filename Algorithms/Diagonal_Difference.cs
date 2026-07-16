e3f837e0a076704254237092f6cc732dfcdf03bf
Sajjadul Islam <sajjadul625@gmail.com>
2026-07-16 10:42:09 +0600

    Diagonal Difference

--- Algorithms/Diagonal_Difference.cs
+++ Algorithms/Diagonal_Difference.cs
@@ -1,1 +1,43 @@
-git 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Result
+{
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int primaryDiagonalSum = 0;
+        int secondaryDiagonalSum = 0;
+        int n = arr.Count;
+
+        for (int i = 0; i < n; i++)
+        {
+            primaryDiagonalSum += arr[i][i];
+
+            secondaryDiagonalSum += arr[i][n - 1 - i];
+        }
+
+        return Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        Console.WriteLine(result);
+    }
+}
